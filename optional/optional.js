dc8066417f829a346c2958c456d3f73042f0cb8c
Advik <katiyar.advik07@gmail.com>
2020-11-27 11:53:56 +0530

    Add files via upload

---
+++ optional/optional.js
@@ -0,0 +1,39 @@
+/*<a name="biography"></a>
+<div class="biography">
+    <div class="bh2">
+        <h2>BIOGRAPHY</h2>
+    </div>
+    <br>
+    <img src="images/bendBio.jpg" width="560" height="715">
+    <div class="bh4">
+        <a href="biography.html">
+            <h4>MORE ABOUT AKSHITA</h4>
+        </a>
+    </div>
+</div>
+
+
+.biography{
+    padding: 20px;
+    background-color: #6699cc;
+}
+
+.biography > img{
+    margin-left: 100px;
+    border-radius: 50px;
+    border-width: 4px;
+    border-color: white;
+}
+
+.bh2{
+    margin-left: 50px;
+    color: white;
+}
+
+.bh4{
+    margin-left: 1200px;
+    color: whitesmoke;
+    font-family: sans-serif;
+}
+
+*/
